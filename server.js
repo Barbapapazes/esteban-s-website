cb15019a7f447c56ea2030fb91cc8be00c0f0113
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-02-18 15:57:47 +0100

    add const port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const PORT = process.env.PORT || 5000
+
 let http = require('http')
 
 http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" })
     res.write('Hello World')
     res.end()
-}).listen(5000)
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`))
