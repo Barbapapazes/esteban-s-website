bce7fab687822cb1005a87e7cff6a16f9382239d
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-03-20 09:10:35 +0100

    update manage sass files

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
+app.use('/stylesheets', sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public/stylesheets'),
     indentedSyntax: false,
     // true = .sass and false = .scss
     outputStyle: 'compressed',
