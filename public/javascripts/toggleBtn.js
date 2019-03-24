4031e67bd9b89234a149ec264424c95f1573294e
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-03-24 23:03:56 +0100

    add web storage for theme

--- public/javascripts/toggleBtn.js
+++ public/javascripts/toggleBtn.js
@@ -2,11 +2,52 @@
     let body = document.querySelector('body')
     let switchBtn = document.querySelector('.container__toggle-btn')
 
+    // Web Storage
+    if (!localStorage.getItem('theme')) {
+        populateStorage()
+    } else {
+        setStyle()
+    }
+
+    /*let url = new URL(window.location)
+    let newURL = new URL(url)
+
+    let href = document.querySelectorAll('a[href]')*/
+
     // Change the stat of the button and the theme of the page
     switchBtn.addEventListener('click', switchTheme)
 
     function switchTheme() {
-        this.classList.toggle('active')
         body.classList.toggle('light')
         body.classList.toggle('dark')
+        populateStorage()
+
+
+        /*for (let index = 0; index < href.length; index++) {
+            const element = href[index];
+            element.href += url.search
+
+    }*/
+        /*if (url.searchParams.get('theme') == 'dark') {
+            newURL = url.href.replace("theme=dark", "theme=light")
+            window.location.href = newURL
+        } else {
+            newURL = url.href.replace("theme=light", "theme=dark")
+            window.location.href = newURL
+        }*/
+    }
+
+    function populateStorage() {
+        localStorage.setItem('theme', body.classList.value)
+
+        setStyle()
+    }
+
+    function setStyle() {
+        let currentTheme = localStorage.getItem('theme')
+        body.classList.value = currentTheme
+        if (body.classList.value === 'light')
+            switchBtn.classList.add('active')
+        else
+            switchBtn.classList.remove('active')
     }
